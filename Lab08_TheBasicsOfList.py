95954e3599826bb906b933cc13499eeebf506fe1
Paolo Gio Espiritu <paologioespiritu@gmail.com>
2025-04-26 18:40:42 +0800

    Add files via upload

---
+++ Lab08_TheBasicsOfList.py
@@ -0,0 +1,9 @@
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+hat_list[2] = int(input("Input new middle number: ")) # Step 1 replace middle number
+
+del hat_list[4] # Step 2: write a line of code that removes the last element from the list.
+
+print("lenght of new list: " + str(len(hat_list)))  # Step 3: write a line of code that prints the length of the existing list.
+
+print("hat_list:",(hat_list))
